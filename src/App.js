a2fb3e1ba8fa23ccf4c501ac0e0e7e8bd45fd63e
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-22 14:39:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React , {Component} from 'react';
 import Post from './components/post/post'
 import './App.css';
-import Contact from './components/Contact/Contact/'
+import Contact from './components/Contact/Contact'
 import CreatePost from './components/createPost/createPost'
 import 'bulma/css/bulma.css'
 import axios from 'axios'
