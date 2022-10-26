4fc9b5a7647107dca9da7a020ac02cf926877ee2
Kamal Nanda <kamalnanda45@gmail.com>
2022-10-27 01:07:28 +0530

    converted into functional-component

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -3,22 +3,17 @@ import LikeHolder from '../likeHolder/likeHolder'
 import User from '../user/user'
 import Comment from '../comments/comment/comment'
 import './stylesheet.css'
-class Post extends Component{
-  render(){
-    const data = this.props.data
-    console.log(data)
-    return (
+const Post = (props) => {
+  const data = props.data
+  return 
     	<div className="post-container">
     		<User name = {data.userName} imgLink= {data.userImg}/>
     		<img src={data.postImg} alt={`${data.name} post`} className="post-img"/>
     		<LikeHolder id={data.id}  loadPosts={this.props.loadPosts}/>
     		<div className="caption-holder">
     			<p><strong>{data.userName}</strong> {data.postCaption}</p>
-    		</div>
-    		{/*<Comment data = {data.comments} id={data.id}/>*/}
-    	</div>
-    );
-  }
-}
+    		</div> 
+    	</div> 
+} 
 
 export default Post;
